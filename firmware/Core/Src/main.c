de0197a7c55bf7a2c43d84704e29ab01347634bd
Bilal MARECAR <billal.marecar@gmail.com>
2023-03-31 11:46:47 +0200

    Sémaphores pour la synchronisation

--- firmware/Core/Src/main.c
+++ firmware/Core/Src/main.c
@@ -22,11 +22,11 @@
 #include "usart.h"
 #include "gpio.h"
 
-
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
 #include <stdio.h>
 #include <string.h>
 /* USER CODE END Includes */
@@ -39,17 +39,21 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 #define STACK_SIZE 1000
-#define Delay 500
+#define Delay 1000
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
+
 
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+/* Semaphore */
+SemaphoreHandle_t Semaphore;
 
 /* USER CODE END PV */
 
@@ -58,35 +62,109 @@ void SystemClock_Config(void);
 void MX_FREERTOS_Init(void);
 /* USER CODE BEGIN PFP */
 void LedBlinker (void * pvParameters);
+
+void taskGive(void *pvParameters);
+void taskTake(void *unsued);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-/* Fonction LED Blinker*/
-void LedBlinker (void * pvParameters){
-    
-    int duree = (int) pvParameters;
+
+
+/* Fonction Give*/
+void taskGive(void *pvParameters) {
+
+    int delay = (int) pvParameters;
     uint8_t uartTxBuffer[100];
     char* s = pcTaskGetName(xTaskGetCurrentTaskHandle());
 
-    while (1)
+
+    for(;;)
     {
-      sprintf((char*)uartTxBuffer, "Je suis la tache %s et je m'endors pour %d periodes\n", s, duree);
-      HAL_UART_Transmit(&huart1, uartTxBuffer, strlen((char*)uartTxBuffer), HAL_MAX_DELAY);
-      vTaskDelay(duree/portTICK_PERIOD_MS);
-      HAL_GPIO_TogglePin(LED_GPIO_Port,LED_Pin);
+
+      //Verification creation semaphore
+      if ( Semaphore != NULL)
+      {
+        // Affichage sur console
+        sprintf((char*)uartTxBuffer, "Je suis la tache %s avant de donner le semaphore\n", s);
+        HAL_UART_Transmit(&huart1, uartTxBuffer, strlen((char*)uartTxBuffer), HAL_MAX_DELAY);
+
+        //Prise du semaphore
+        xSemaphoreGive(Semaphore);
+
+        //Verification avec la LED
+        HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,GPIO_PIN_RESET);
+
+        /* Vérification du status binaire du semaphore*/
+        if(uxSemaphoreGetCount(Semaphore) > 0)
+        {
+          sprintf((char*)uartTxBuffer, "Je suis la tache %s apres avoir donner semaphore tous les %d ms\n", s, delay);
+          HAL_UART_Transmit(&huart1, uartTxBuffer, strlen((char*)uartTxBuffer), HAL_MAX_DELAY);
+
+          
+        }
+        
+        //Delais
+        vTaskDelay(delay/portTICK_PERIOD_MS);
+
+      }
+      else
+      {
+        char *strsem = "Erreur : Semaphore non cree\n" ;
+        HAL_UART_Transmit(&huart1, strsem, strlen(strsem), HAL_MAX_DELAY);
+
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        HAL_NVIC_SystemReset();
+
+      }
+      
     }
-    
+
 }
 
-  /* Fonction affichage*/
-  // int __io_putchar(int ch) 
-  // {
-  // uint8_t data = ch;
-  // HAL_UART_Transmit(&huart1, (uint8_t *)&data, 1, HAL_MAX_DELAY);
-  // return ch;
-  // }
+void taskTake(void *unsued) {
+
+    uint8_t uartTxBuffer[100];
+    char* s = pcTaskGetName(xTaskGetCurrentTaskHandle());
+
+    for(;;)
+    {
+
+      //Verification creation semaphore
+      if ( Semaphore != NULL)
+      {
+        // Affichage sur console
+        sprintf((char*)uartTxBuffer, "Je suis la tache %s avant de prendre le semaphore\n", s);
+        HAL_UART_Transmit(&huart1, uartTxBuffer, strlen((char*)uartTxBuffer), HAL_MAX_DELAY);
+
+        //Prise du semaphore
+        xSemaphoreTake(Semaphore,HAL_MAX_DELAY);
+
+        //Verification avec la LED
+        HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin,GPIO_PIN_SET);
+
+        /* Vérification du status binaire du semaphore*/
+        if(uxSemaphoreGetCount(Semaphore) > 0)
+        {
+          sprintf((char*)uartTxBuffer, "Je suis la tache %s apres avoir pris le semaphore \n", s);
+          HAL_UART_Transmit(&huart1, uartTxBuffer, strlen((char*)uartTxBuffer), HAL_MAX_DELAY);
+
+          
+        }
+
+      }
+      else
+      {
+        char *strsem = "Erreur : Semaphore non cree\n" ;
+        HAL_UART_Transmit(&huart1, strsem, strlen(strsem), HAL_MAX_DELAY);
+
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        HAL_NVIC_SystemReset();
+    }
+  }
+}
+
 
 /* USER CODE END 0 */
 
@@ -99,6 +177,9 @@ int main(void)
   /* USER CODE BEGIN 1 */
   BaseType_t xReturned;
   TaskHandle_t xHandle1 = NULL;
+
+  TaskHandle_t xHandlegive = NULL;
+  TaskHandle_t xHandletake = NULL;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -121,15 +202,30 @@ int main(void)
   MX_GPIO_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-  /*Create a task*/
-  xReturned = xTaskCreate(
-          LedBlinker,      // Function to be called
-          "LedBlink",     // Name of task
+
+  // Create binary semaphore before starting task
+  Semaphore = xSemaphoreCreateBinary(); 
+
+  /* Create a taskGive*/
+  xTaskCreate(
+          taskGive,       // Function to be called
+          "Give",         // Name of task
           STACK_SIZE,     // Stack size
           (void*) Delay,  // Parameter to pass to function
           1,              // Task priority 0 to configMAX_PRIORITIES - 1 (FreeRTOSConfig.h)
-          &xHandle1       // Task handle (allows to find and manipulate the task)
+          &xHandlegive       // Task handle (allows to find and manipulate the task)
           );
+
+  /* Create a taskTake*/
+  xTaskCreate(
+          taskTake,       // Function to be called
+          "Take",         // Name of task
+          STACK_SIZE,     // Stack size
+          NULL,           // Parameter to pass to function
+          2,              // Task priority 0 to configMAX_PRIORITIES - 1 (FreeRTOSConfig.h)
+          &xHandletake       // Task handle (allows to find and manipulate the task)
+          );
+
   /* USER CODE END 2 */
 
   /* Call init function for freertos objects (in freertos.c) */
